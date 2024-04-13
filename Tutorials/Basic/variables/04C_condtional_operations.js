21d52645ad2671668b52d51a939c64258b6b3cb8
prakarsh17 <88645086+infinotiver@users.noreply.github.com>
2024-04-13 16:49:18 +0530

    [Add] conditional operators

---
+++ Tutorials/Basic/variables/04C_condtional_operations.js
@@ -0,0 +1,13 @@
+// Conditional (or ternary) Operators
+/**
+ * This file demonstrates the use of conditional or ternary operators.
+ * 
+ * Conditional or ternary operators are a shorthand for if-else statements and are denoted by the ? : syntax.
+ * They consist of three parts: a condition, a value if the condition is true, and a value if the condition is false.
+ * The condition is evaluated and if it is true, the first value is returned, otherwise the second value is returned.
+ * Syntax:
+ *           variable = (condition) ? TRUE_value: FALSE_value
+ */
+let age = 18;
+let canVote = (age >= 18) ? "Yes" : "No"; // ternary operator
+console.log(canVote)
