b31abe67ce0221c3a79013401ccc7bb82ecaac24
infinotiver <88645086+infinotiver@users.noreply.github.com>
2024-04-13 17:54:11 +0530

    Add debug statemetns

--- generate_toc.js
+++ generate_toc.js
@@ -1,5 +1,3 @@
-// generate_toc.js
-
 const fs = require('fs');
 const path = require('path');
 
@@ -13,6 +11,8 @@ function generateTableOfContents(directory) {
    * @param {number} depth - The depth of the current path.
    */
   function traverseDirectory(currentPath, depth) {
+    console.log(`Traversing directory: ${currentPath}`); // Log the current directory
+
     // Get the files in the current directory
     const files = fs.readdirSync(currentPath);
 
@@ -24,24 +24,28 @@ function generateTableOfContents(directory) {
       // Get the stats of the file
       const stats = fs.statSync(filePath);
 
+      console.log(`Processing file: ${filePath}`); // Log the processed file
+
       // If the file is a directory, generate the table of contents recursively
       if (stats.isDirectory()) {
         // Capitalize the first letter of the directory name
         const folderName = file.charAt(0).toUpperCase() + file.slice(1);
 
+        console.log(`Adding folder to TOC: ${folderName}`); // Log added folder
+
         // Add the folder name to the table of contents with appropriate indentation
-        tableOfContents += `${"    ".repeat(depth)}- ${folderName}\n`;
+        tableOfContents += `${"  ".repeat(depth)}- ${folderName}\n`;
 
         // Recursively traverse the directory
         traverseDirectory(filePath, depth + 1);
-      } 
-      // If the file is a JavaScript file, generate a link to it in the table of contents
-      else if (file.endsWith(".js")) {
+      } else if (file.endsWith(".js")) {
         // Get the relative path of the file
         const relativePath = path.relative(directory, filePath).replace(/\\/g, "/");
 
+        console.log(`Adding JS file to TOC: ${file} - ${relativePath}`); // Log added JS file
+
         // Add a link to the file in the table of contents with appropriate indentation
-        tableOfContents += `${"    ".repeat(depth)}- [${file}](${relativePath})\n`;
+        tableOfContents += `${"  ".repeat(depth)}- [${file}](${relativePath})\n`;
       }
     }
   }
@@ -60,7 +64,7 @@ const readmePath = path.join(process.env.GITHUB_WORKSPACE, 'README.md');
 let readmeContent = fs.readFileSync(readmePath, 'utf8');
 
 const tocHeader = '## Table of Contents';
-const tocMarker = '<!-- TABLE_OF_CONTENTS_MARKER -->';
+const tocMarker = '';
 const updatedReadmeContent = readmeContent.replace(tocMarker, `${tocHeader}\n\n${tableOfContents}\n\n${tocMarker}`);
 
 fs.writeFileSync(readmePath, updatedReadmeContent);
