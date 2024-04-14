2c2beac28fc1d198759406c4a40e361a663283cb
infinotiver <88645086+infinotiver@users.noreply.github.com>
2024-04-14 19:24:48 +0530

    get absolute path

--- generate_toc.js
+++ generate_toc.js
@@ -34,18 +34,18 @@ function generateTableOfContents(directory) {
         console.log(`Adding folder to TOC: ${folderName}`); // Log added folder
 
         // Add the folder name to the table of contents with appropriate indentation
-        tableOfContents += `${"  ".repeat(depth)}- ${folderName}\n`;
+        tableOfContents += `${" ".repeat(depth)}- ${folderName}\n`;
 
         // Recursively traverse the directory
         traverseDirectory(filePath, depth + 1);
       } else if (file.endsWith(".js")) {
-        // Get the relative path of the file
-        const relativePath = path.relative(directory, filePath).replace(/\\/g, "/");
+        // Get the absolute path of the file
+        const absolutePath = path.join(directory, filePath);
 
-        console.log(`Adding JS file to TOC: ${file} - ${relativePath}`); // Log added JS file
+        console.log(`Adding JS file to TOC: ${file} - ${absolutePath}`); // Log added JS file
 
         // Add a link to the file in the table of contents with appropriate indentation
-        tableOfContents += `${"  ".repeat(depth)}- [${file}](${relativePath})\n`;
+        tableOfContents += `${" ".repeat(depth)}- [${file}](${absolutePath})\n`;
       }
     }
   }
@@ -64,7 +64,7 @@ const readmePath = path.join(process.env.GITHUB_WORKSPACE, 'README.md');
 console.log(readmePath);
 let readmeContent = fs.readFileSync(readmePath, 'utf8');
 const tocHeader = '## Table of Contents';
-const tocMarker = '<!-- TABLE_OF_CONTENTS_MARKER -->';
+const tocMarker = '';
 const updatedReadmeContent = readmeContent.replace(tocMarker, `${tocHeader}\n\n${tableOfContents}\n\n${tocMarker}`);
 console.log("Update content ",updatedReadmeContent);
 console.log("Content before TOC:", updatedReadmeContent.slice(0, 100)); // First 100 characters
@@ -78,4 +78,3 @@ try {
   console.error("Error writing to README file:", error);
   // Handle the error (e.g., fail the job, log a message)
 }
-
