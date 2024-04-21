0cecc03b7ff29b5ebfa97ebfd87cea09e2242570
prakarsh17 <88645086+infinotiver@users.noreply.github.com>
2024-04-21 20:39:17 +0530

    Update generate_toc.js

--- generate_toc.js
+++ generate_toc.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-function generateTableOfContents(directory) {
+function generateTableOfContents(directory, depth = 0) {
     // Initialize the table of contents with an empty string
     let tableOfContents = "";
 
@@ -14,16 +14,18 @@ function generateTableOfContents(directory) {
 
             if (stats.isDirectory()) {
                 const folderName = file.charAt(0).toUpperCase() + file.slice(1);
-                tableOfContents += `${" ".repeat(depth)}- ${folderName}\n`;
+                console.log(`Adding folder to TOC: ${folderName}`);
+                tableOfContents += `${" ".repeat(depth * 2)}- ${folderName}\n`;
                 traverseDirectory(filePath, depth + 1);
             } else if (file.endsWith(".js")) {
                 const absolutePath = path.join(directory, filePath);
-                tableOfContents += `${"     ".repeat(depth)}- [${file}](${absolutePath})\n`;
+                console.log(`Adding JS file to TOC: ${file} - ${absolutePath}`);
+                tableOfContents += `${" ".repeat((depth + 1) * 2)}- [${file}](${absolutePath})\n`;
             }
         }
     }
 
-    traverseDirectory(directory, 0);
+    traverseDirectory(directory, depth);
 
     return tableOfContents;
 }
@@ -36,13 +38,12 @@ console.log(readmePath);
 let readmeContent = fs.readFileSync(readmePath, 'utf8');
 const tocMarker = '<!-- INSERT_TOC_HERE -->'; // Unique marker to indicate where the TOC should be inserted
 
-// Find the first occurrence of the tocMarker in the README content
-const markerIndex = readmeContent.indexOf(tocMarker);
+const startIndex = readmeContent.indexOf(tocMarker);
+const endIndex = startIndex + tocMarker.length;
 
-if (markerIndex !== -1) {
-    // Insert the generated table of contents below the tocMarker
-    const updatedContent = readmeContent.slice(0, markerIndex + tocMarker.length) + '\n\n' + tableOfContents + readmeContent.slice(markerIndex + tocMarker.length);
-    console.log(updatedContent);
+if (startIndex !== -1 && endIndex !== -1) {
+    // Replace the tocMarker with the generated table of contents
+    const updatedContent = readmeContent.slice(0, startIndex) + tableOfContents + readmeContent.slice(endIndex);
 
     // Write the updated content back to the README file using writeFile
     fs.writeFile(readmePath, updatedContent, { encoding: 'utf8' }, (err) => {
@@ -50,7 +51,6 @@ if (markerIndex !== -1) {
             console.error("Error writing to README file:", err);
         } else {
             console.log("README file updated successfully!");
-
         }
     });
 } else {
