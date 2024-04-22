776d27129913fb9a3eacfa08ebd0bb1fad0d8629
prakarsh17 <88645086+infinotiver@users.noreply.github.com>
2024-04-22 17:50:02 +0530

    [Add] How to use hasOwnProperty method in javascript

---
+++ Tutorials/Basic/objects/07_hasOwnProperty.js
@@ -0,0 +1,16 @@
+// 07_hasOwnProperty.js
+/*
+This code snippet demonstrates how to use the hasOwnProperty method
+*/
+
+var Room = {
+    name: "Living Room",
+    width: 10,
+    height: 15,
+}
+
+console.log(Room.hasOwnProperty("length")) // Output: false
+
+Room.length = 20
+
+console.log(Room.hasOwnProperty("length")) // Output: true
