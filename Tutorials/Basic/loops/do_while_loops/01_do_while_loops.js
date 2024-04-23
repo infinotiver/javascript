323be27779b44ef981b61302a394d5f10ba3aa58
prakarsh17 <88645086+infinotiver@users.noreply.github.com>
2024-04-23 19:23:22 +0530

    [Add] Basic introduction to do while loops

---
+++ Tutorials/Basic/loops/do_while_loops/01_do_while_loops.js
@@ -0,0 +1,17 @@
+// 01_do_while_loops.js
+/* 
+This code snippet shows the usage of do.. while loops in JavaScript
+*/
+/* SYNTAX
+
+do {
+    code 
+} while (condition)
+*/
+
+let myVar = 10;
+
+do {
+    myVar ++;
+    console.log(myVar);
+} while (myVar  < 10)
