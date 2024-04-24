37e19d2529cfc99de52bc40a84beedeb6b838374
prakarsh17 <88645086+infinotiver@users.noreply.github.com>
2024-04-24 16:43:42 +0530

    [Add] basic introduction to Math

---
+++ Tutorials/Intermediate/Math/01_basic.js
@@ -0,0 +1,21 @@
+// 01_basic.js
+/*
+Math is a built-in object that provides properties and methods for mathematical operations. It is a global object and is available in all JavaScript environments without the need to create an instance.
+
+Properties: 
+    The Math object has several properties that represent mathematical constants and values. Some examples include Math.PI, which represents the mathematical constant pi, and Math.E, which represents the base of the natural logarithm, approximately equal to 2.71828.
+
+Methods: 
+    The Math object provides various methods for performing mathematical operations. Some commonly used methods include Math.abs(), which returns the absolute value of a number, Math.round(), which rounds a number to the nearest integer, and Math.random(), which generates a random number between 0 (inclusive) and 1 (exclusive).
+    
+Mathematical Functions:
+    The Math object also provides methods for various mathematical functions, such as Math.sqrt() for calculating the square root, Math.pow() for raising a number to a power, Math.sin(), Math.cos(), Math.tan(), Math.asin(), Math.acos(), Math.atan(), and more.
+*/
+console.log("Math.LN10: " + Math.LN10);
+console.log("Math.LOG2E: " + Math.LOG2E);
+console.log("Math.Log10E: " + Math.LOG10E);
+console.log("Math.SQRT2: " + Math.SQRT2);
+console.log("Math.SQRT1_2: " + Math.SQRT1_2);
+console.log("Math.LN2: " + Math.LN2);
+console.log("Math.E: " + Math.E);
+console.log("Math.PI: " + Math.PI);
