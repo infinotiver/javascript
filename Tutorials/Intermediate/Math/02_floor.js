396c673b9eb09840f0a9633f5248c4b0eeb4f41a
prakarsh17 <88645086+infinotiver@users.noreply.github.com>
2024-05-04 17:38:28 +0530

    [Add] Math.floor usage

---
+++ Tutorials/Intermediate/Math/02_floor.js
@@ -0,0 +1,16 @@
+//02_floor.js
+// Rounds a number down to the nearest smaller integer
+// Using Math.floor()
+//
+// Parameters:
+// - num (number): The number to be rounded.
+//
+// Returns:
+// - The rounded number (number).
+
+console.log(floor(1249.59)); // Output: 1249
+
+function floor(num) {
+  return Math.floor(num);
+}
+
