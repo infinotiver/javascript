070c872990daeb9137631eef2ac2d6a723cea9f5
prakarsh17 <88645086+infinotiver@users.noreply.github.com>
2024-05-04 18:45:08 +0530

    [Add] Math.round

---
+++ Tutorials/Intermediate/Math/03_round.js
@@ -0,0 +1,7 @@
+// 03_round.js
+/* This file shows the usage of Math.round method in JavaScript
+Math.round is used to round off any number to the nearest integer (either big or smaller)
+*/
+
+console.log(Math.round(1249.59)) // Output: 1250
+
