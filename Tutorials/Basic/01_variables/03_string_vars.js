d7f93b52a1fb075d10d6362f645713b72219b7ef
prakarsh17 <88645086+infinotiver@users.noreply.github.com>
2024-05-07 19:06:33 +0530

    Minor updates, add definition of CONCATENATION

--- Tutorials/Basic/01_variables/03_string_vars.js
+++ Tutorials/Basic/01_variables/03_string_vars.js
@@ -1,14 +1,18 @@
 // 03_string_vars.js
 
 // Declaring and initializing first name and last name
-var firstName = "Pranjal";
-var lastName = "Prakarsh";
+var firstName = "Max";
+var lastName = "Barry";
 
 // Concatenating strings
+/* Concatenating refers to linking two things together, in JavaScript, concatenation of strings simply means to link any two strings together.
+For example:
+    const fullSentence = "This is sentence 1" + "This is Sentence 2"
+*/
 var fullName = firstName + " " + lastName;
 
 // Concatenating strings using template literals
-var fullNameTemplate = `${firstName} ${lastName}`;
+var fullNameTemplate = `${firstName} ${lastName}`; // PREFERRED 
 // Concatenating strings (EASIER)
 var fullName = firstName + " " + lastName;
 
